38d8bd5973a0f082c4b4f4debeb8e971bb01e476
kai <b10223052@yuntech.org.tw>
2019-10-27 19:12:58 +0800

    add modal component

---
+++ src/components/modal/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '../button';
+import cx from 'classnames';
+import './style.scss';
+
+const PREFIX_CLASS = 'modal';
+
+const propTypes = {
+	message: PropTypes.oneOfType([
+		PropTypes.node,
+		PropTypes.string,
+	]),
+	onReset: PropTypes.func,
+	onVisible: PropTypes.func,
+	isVisible: PropTypes.bool,
+};
+
+function Modal({ isVisible ,message, onReset, onVisible }) {
+	function _handleClick() {
+		onReset();
+		onVisible();
+	}
+	return (
+		<div className={cx(PREFIX_CLASS, { [`${PREFIX_CLASS}__open`]: isVisible })}>
+			<div className={`${PREFIX_CLASS}__content`}>
+				<p>
+					{message}
+				</p>
+				<Button onClick={_handleClick}>Reset</Button>
+			</div>
+		</div>
+	);
+}
+
+Modal.propTypes = propTypes;
+
+export default Modal;
