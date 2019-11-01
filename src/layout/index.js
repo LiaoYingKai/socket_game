538365d84e401ab2c8b6593d00a51ee675057be6
kai <b10223052@yuntech.org.tw>
2019-11-01 15:08:01 +0800

    get play role

--- src/layout/index.js
+++ src/layout/index.js
@@ -24,7 +24,8 @@ const propTypes = {};
 
 function Layout() {
 	const [checkerboard, setCheckerboard] = useState(initCheckerboard);
-	const [play, setPlay] = useState(PLAY_1);
+	const [play, setPlay] = useState('');
+	const [nowPlay, setNowPlay] = useState('');
 	const [message, setMessage] = useState('');
 	const [isVisible, setVisible] = useState(false);
 	const [playStatus, setPlayStatus] = useState(NONE);
@@ -48,53 +49,52 @@ function Layout() {
 				// 開始遊戲
 				setPlayStatus(PLAYING);
 			});
+			// 玩家角色
+			ws.on('PLAY_1', () => {
+				setPlay(PLAY_1);
+			});
+			ws.on('PLAY_2', () => {
+				setPlay(PLAY_2);
+			});
+			// 現在是哪一個玩家下棋
+			ws.on('nowPlay', (nowPlay) => {
+				setNowPlay(nowPlay);
+			});
+			ws.on('updateChess',() => {
+				// 更新棋盤
+			});
+			ws.on('errorAdd', () => {
+				// 不能下的地方
+			});
+			ws.on('gameResult', () => {
+				// 判斷勝利
+			});
 			ws.on('leaveGame', () => {
 				// 離開遊戲
 				setPlayStatus(NONE);
 			});
 		}
 	},[ws]);
-	// useEffect(() => {
-	// 	connectWebSocket();
-	// },[]);
-
-	// useEffect(() => {
-	// 	if (ws) {
-	// 		//連線成功在 console 中打印訊息
-	// 		console.log('success connect!');
-	// 		//設定監聽
-	// 		initWebSocket();
-	// 	}
-	// },[ws]);
-
-	// const initWebSocket = () => {
-	// 	//對 getMessage 設定監聽，如果 server 有透過 getMessage 傳送訊息，將會在此被捕捉
-	// 	ws.on('getMessage', message => {
-	// 		console.log(message);
-	// 	});
-	// };
-
-	// const sendMessage = () => {
-	// 	//以 emit 送訊息，並以 getMessage 為名稱送給 server 捕捉
-	// 	ws.emit('getMessage', '只回傳給發送訊息的 client');
-	// };
 
 	function _handelClick(rowIndex, columnIndex) {
-		if (checkerboard[rowIndex][columnIndex]) return;
-		const newCheckerboard = checkerboard.map(row => row.map(item => item));
+		if (!ws) return;
+		// 下棋
+		ws.emit('addChess', { rowIndex, columnIndex, });
+		// if (checkerboard[rowIndex][columnIndex]) return;
+		// const newCheckerboard = checkerboard.map(row => row.map(item => item));
 
-		newCheckerboard[rowIndex][columnIndex] = play;
-		setCheckerboard(newCheckerboard);
-		_handleChangePlay();
+		// newCheckerboard[rowIndex][columnIndex] = play;
+		// setCheckerboard(newCheckerboard);
+		// _handleChangePlay();
 	}
 
-	function _handleChangePlay() {
-		if (play === PLAY_1) {
-			setPlay(PLAY_2);
-		} else {
-			setPlay(PLAY_1);
-		}
-	}
+	// function _handleChangePlay() {
+	// 	if (play === PLAY_1) {
+	// 		setPlay(PLAY_2);
+	// 	} else {
+	// 		setPlay(PLAY_1);
+	// 	}
+	// }
 
 	function _handleJudgeVictory(play) {
 		let isWin = false;
@@ -155,7 +155,8 @@ function Layout() {
 			case PLAYING: {
 				return (
 					<div className="title">
-						換 <span className={`title--${play}`}>{play}</span> 下
+						你是 <span className={`title--${play}`}>{play}</span>
+						現在是 <span className={`title--${nowPlay}`}>{nowPlay}</span> 下
 					</div>
 				);
 			}
