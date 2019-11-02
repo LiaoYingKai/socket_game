db9e303d710fcf2e5fafb5bfa1d326e33cdbbef1
kai <b10223052@yuntech.org.tw>
2019-11-03 02:29:05 +0800

    add sample lobby

---
+++ src/Pages/lobby/index.js
@@ -0,0 +1,16 @@
+import React, { useState, } from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+function Main() {
+	return (
+		<div>
+			Hello World
+		</div>
+	);
+}
+
+Main.propTypes = propTypes;
+
+export default Main;
