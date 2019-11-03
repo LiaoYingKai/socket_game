68e4e7d7384839cb17d3930263fb7e0b663a2bac
kai <b10223052@yuntech.org.tw>
2019-11-03 11:28:15 +0800

    add lobby html and css style

--- src/Pages/lobby/index.js
+++ src/Pages/lobby/index.js
@@ -1,16 +1,32 @@
 import React, { useState, } from 'react';
 import PropTypes from 'prop-types';
+import { PathEnums } from '../../route/config';
+import ImageLink from '../../components/image-link';
+import './style.scss';
+
+const {
+	TIC_TAC_TOC,
+	GOMOKU
+} = PathEnums;
 
 const propTypes = {};
 
-function Main() {
+function Lobby() {
 	return (
-		<div>
-			Hello World
+		<div className="lobby">
+			<div className="lobby__title"> 請選擇遊戲 </div>
+			<div className="lobby__content">
+				<ImageLink
+					imageSrc={'https://lh3.googleusercontent.com/AswZr3s2goU6BxNLCcPkV9AANJEIGh7we_8tv5LS7d6vCiyTGeS67nVfVSbZJEzC4mw'}
+				/>
+				<ImageLink
+					imageSrc={'http://587.renju.org.tw/p/4.jpg'}
+				/>
+			</div>
 		</div>
 	);
 }
 
-Main.propTypes = propTypes;
+Lobby.propTypes = propTypes;
 
-export default Main;
+export default Lobby;
