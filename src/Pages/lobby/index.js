ad41bb619340c1ba78d813f8b94d4ca91caf342d
kai <b10223052@yuntech.org.tw>
2019-11-03 11:41:23 +0800

    add lobby page

--- src/Pages/lobby/index.js
+++ src/Pages/lobby/index.js
@@ -1,4 +1,4 @@
-import React, { useState, } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { PathEnums } from '../../route/config';
 import ImageLink from '../../components/image-link';
@@ -9,18 +9,22 @@ const {
 	GOMOKU
 } = PathEnums;
 
-const propTypes = {};
+const propTypes = {
+	history: PropTypes.object,
+};
 
-function Lobby() {
+function Lobby({ history }) {
 	return (
 		<div className="lobby">
 			<div className="lobby__title"> 請選擇遊戲 </div>
 			<div className="lobby__content">
 				<ImageLink
 					imageSrc={'https://lh3.googleusercontent.com/AswZr3s2goU6BxNLCcPkV9AANJEIGh7we_8tv5LS7d6vCiyTGeS67nVfVSbZJEzC4mw'}
+					onClick={() => {history.push(TIC_TAC_TOC);}}
 				/>
 				<ImageLink
 					imageSrc={'http://587.renju.org.tw/p/4.jpg'}
+					onClick={() => {history.push(GOMOKU);}}
 				/>
 			</div>
 		</div>
