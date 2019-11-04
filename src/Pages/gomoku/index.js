01521f2a3dabf93cb7c9566302f5c85b822c4165
kai <b10223052@yuntech.org.tw>
2019-11-04 17:53:36 +0800

    add socket connection

--- src/Pages/gomoku/index.js
+++ src/Pages/gomoku/index.js
@@ -2,7 +2,11 @@ import React, { useState, } from 'react';
 import PropTypes from 'prop-types';
 import PlayTitle from '../../components/play-title';
 import { PlayEnums, PlayStatus } from '../../enums';
+import webSocket from 'socket.io-client';
 import Lattice from '../../components/lattice';
+import Message from '../../components/message';
+import Modal from '../../components/modal';
+import Button from '../../components/button';
 import Checkerboard from '../../components/checkerboard';
 import './style.scss';
 
@@ -22,31 +26,84 @@ const {
 } = GomokuStatusEnums;
 
 const propTypes = {};
-const initCheckerboard = [['x','o','x','o','o','o','x','x','o','x','o','x','x','o','x'],['x','o','x','o','o','o','x','x','o','x','o','x','x','o','x'],['x','o','x','o','o','o','x','x','o','x','o','x','x','o','x'],['x','o','x','o','o','o','x','x','o','x','o','x','x','o','x'],['x','o','x','o','o','o','x','x','o','x','o','x','x','o','x'],['x','o','x','o','o','o','x','x','o','x','o','x','x','o','x'],['x','o','x','o','o','o','x','x','o','x','o','x','x','o','x'],['x','o','x','o','o','o','x','x','o','x','o','x','x','o','x'],['x','o','x','o','o','o','x','x','o','x','o','x','x','o','x'],['x','o','x','o','o','o','x','x','o','x','o','x','x','o','x'],['x','o','x','o','o','o','x','x','o','x','o','x','x','o','x'],['x','o','x','o','o','o','x','x','o','x','o','x','x','o','x'],['x','o','x','o','o','o','x','x','o','x','o','x','x','o','x'],['x','o','x','o','o','o','x','x','o','x','o','x','x','o','x'],['x','o','x','o','o','o','x','x','o','x','o','x','x','o','x']];
+const initCheckerboard = createCheckerboard();
+// const socketUrl = 'https://kai-game-server.herokuapp.com/tic-tac-toc';
+const socketUrl = 'http://localhost:8888/gomoku';
 
+function createCheckerboard() {
+	let checkerboard = [];
+
+	for (let i = 0; i < 20; i++) {
+		checkerboard[i] = [];
+		for (let j = 0; j < 20; j++) {
+			checkerboard[i][j] = '';
+		}
+	}
+	return checkerboard;
+}
 function Gomoku() {
 	const [playStatus, setPlayStatus] = useState(NONE);
+	const [modalMessage, setModalMessage] = useState('');
+	const [isModalVisible, setModalVisible] = useState(false);
+	const [message, setMessage] = useState('');
+	const [isMessageVisible, setMessageVisible] = useState(false);
+	const [ws,setWs] = useState(null);
+
+	function _handleConnectWebSocket() {
+		setWs(webSocket(socketUrl));
+		setPlayStatus(LOADING);
+	}
+
+	function _handleDisconnectWebSocket() {
+		ws.close();
+		setPlayStatus(NONE);
+	}
+	function _handlePlayChess(rowIndex, columnIndex) {
+		console.log(rowIndex, columnIndex);
+	}
+	function _handleClickBack() {
+		// if (!ws) return;
+
+		// ws.emit('init');
+		// setPlayStatus(NONE);
+		// setModalVisible(false);
+	}
+
+	function _renderButton() {
+		if (playStatus === NONE) {
+			return <Button 
+				onClick={_handleConnectWebSocket}
+			> 開始遊戲 </Button>;
+		} 
+		if (playStatus === LOADING) {
+			return <Button 
+				onClick={_handleDisconnectWebSocket}
+			> 取消遊戲 </Button>;
+		}
+	}
 
 	return (
 		<div className="gomoku">
 			<PlayTitle
 				status={playStatus}
 			/>
-			{/* <Lattice.Gomoku/>
-			<Lattice.Gomoku
-				status={Lattice.Gomoku.GomokuStatusEnums.BLOCK}
-			/>
-			<Lattice.Gomoku
-				status={Lattice.Gomoku.GomokuStatusEnums.WHITE}
-			/>
-			<Lattice.Gomoku/> */}
 			<Checkerboard
 				checkerboard={initCheckerboard}
-				// onClick={_handlePlayChess}
+				onClick={_handlePlayChess}
 				Lattice={Lattice.Gomoku}
 				play1={BLOCK}
 				play2={WHITE}
 				type={Checkerboard.TypeEnums.GOMOKU}
+			/>
+			{_renderButton()}
+			<Modal 
+				onClick={_handleClickBack}
+				isVisible={isModalVisible}
+				message={modalMessage}
+			/>
+			<Message
+				isVisible={isMessageVisible}
+				message={message}
 			/>
 		</div>
 	);
