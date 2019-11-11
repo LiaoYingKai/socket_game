38f944b89769e91b06721e08d4cd23b145f72f8b
kai <b10223052@yuntech.org.tw>
2019-11-11 17:46:26 +0800

    add button type enums

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -3,17 +3,28 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import './style.scss';
 
+const TypeEnums = {
+	SOLID: 'solid',
+	HOLLOW: 'hollow',
+};
+
 const propTypes = {
 	onClick: PropTypes.func,
 	children: PropTypes.string,
 	className: PropTypes.string,
+	type: PropTypes.oneOf(Object.values(TypeEnums))
 };
 
-function Button({ onClick, children, className }) {
+const defaultProps = {
+	onClick: () => {},
+	type: TypeEnums.SOLID
+};
+
+function Button({ onClick, children, className, type }) {
 	return (
 		<button 
 			onClick={onClick}
-			className={cx(className, 'button')}
+			className={cx(className, 'button', `button--${type}`)}
 		>
 			{children}
 		</button>
@@ -21,5 +32,7 @@ function Button({ onClick, children, className }) {
 }
 
 Button.propTypes = propTypes;
+Button.defaultProps = defaultProps;
+Button.TypeEnums = TypeEnums;
 
 export default Button;
