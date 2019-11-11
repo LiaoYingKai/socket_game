7574964c3380d34654c2f24878d2d574b5129338
kai <b10223052@yuntech.org.tw>
2019-11-11 16:42:44 +0800

    add poker-boast-game config

---
+++ src/Pages/poker-boast/game.js
@@ -0,0 +1,16 @@
+import React, { useState, } from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+function PokerBoastGame() {
+	return (
+		<div>
+			Hello World aa
+		</div>
+	);
+}
+
+PokerBoastGame.propTypes = propTypes;
+
+export default PokerBoastGame;
--- src/route/config.js
+++ src/route/config.js
@@ -4,19 +4,22 @@ const Lobby = loadComponent({ loader: () => import('../Pages/lobby') });
 const TicTacToc = loadComponent({ loader: () => import('../Pages/tic-tac-toc') });
 const Gomoku = loadComponent({ loader: () => import('../Pages/gomoku') });
 const PokerBoast = loadComponent({ loader: () => import('../Pages/poker-boast') });
+const PokerBoastGame = loadComponent({ loader: () => import('../Pages/poker-boast/game') });
 
 const PathEnums = {
 	LOBBY: '/',
 	TIC_TAC_TOC: '/tic-tac-toc',
 	GOMOKU: '/gomoku',
 	POKER_BOAST: '/poker-boast',
+	POKER_BOAST_GAME: '/poker-boast-game',
 };
 
 const {
 	LOBBY,
 	TIC_TAC_TOC,
 	GOMOKU,
-	POKER_BOAST
+	POKER_BOAST,
+	POKER_BOAST_GAME
 } = PathEnums;
 
 export const config = [
@@ -38,6 +41,11 @@ export const config = [
 	{
 		path: POKER_BOAST,
 		component: PokerBoast,
+		exact: true,
+	},
+	{
+		path: POKER_BOAST_GAME,
+		component: PokerBoastGame,
 		exact: true,
 	},
 ];
