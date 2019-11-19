4c212009906b4976ede6ea018cb6b05a94227106
kai <b10223052@yuntech.org.tw>
2019-11-19 18:29:16 +0800

    add background

--- src/Pages/90s-game/index.js
+++ src/Pages/90s-game/index.js
@@ -1,14 +1,19 @@
 import React, { useState, } from 'react';
 import PropTypes from 'prop-types';
+import BackgroundRoad from '../../images/90s-game/background-road.png';
+import BackgroundDashLine from '../../images/90s-game/background-dash-line.png';
 import './style.scss';
+
+const PREFIX_CLASS = "game-of-90s";
 
 const propTypes = {};
 
 function GameOf90s() {
 	return (
-		<div>
-			Hello World
+		<div className={PREFIX_CLASS}>
+			<img src={BackgroundDashLine} className={`${PREFIX_CLASS}__dash-line`}></img>
 		</div>
+
 	);
 }
 
